a6ca20d5554d31188979ea6cfa8b5d14784ecd2a
felipeLeao18 <flpleao1807@gmail.com>
2022-11-12 14:08:23 -0300

    feat: add zod middleware and user router

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response, NextFunction } from 'express'
 import 'express-async-errors'
 import cors from 'cors'
+import { router as userRouter } from './controllers/user/router'
 
 const app = express()
 app.use(express.json())
 
 app.use(cors())
+app.use(userRouter)
 app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
   if (err.err && err.err instanceof Error) {
     return res.status(err.statusCode).json({
@@ -13,11 +15,22 @@ app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
       status: 'error'
     })
   }
-
+  if (err.errors) {
+    const { errors } = err
+    const errorMessages = errors.map((error: { code: string, expected: string, received: string, path: string[], message: string }) => {
+      if (error.message !== 'Required') {
+        return error.message
+      }
+      return `${error.path} is required`
+    })
+    return res.status(422).send({
+      message: errorMessages,
+      status: 'invalidSchemaError'
+    })
+  }
   return res.status(500).json({
     status: 'error',
     message: 'Internal server error'
   })
 })
-
 export { app }

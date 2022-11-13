71dd77c11d703b137ab9c87d9953b38917566c62
felipeLeao18 <flpleao1807@gmail.com>
2022-11-13 16:18:11 -0300

    feat: add validate hash to crypt lib to check password when user login

--- src/services/common/crypt.ts
+++ src/services/common/crypt.ts
@@ -1,10 +1,15 @@
 import bcrypt from 'bcrypt'
 
-const createHash = (data: string) => {
+const createHash = (data: string): string => {
   const hash = bcrypt.hashSync(data, 10)
   return hash
 }
 
+const validateHash = (data: string, dataHash: string): boolean => {
+  const validHash: boolean = bcrypt.compareSync(data, dataHash)
+  return validHash
+}
 export const crypt = {
-  createHash
+  createHash,
+  validateHash
 }

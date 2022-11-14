2280c8b54d05c6f61f8ea81d8e0d6b0c478d7435
flp <flpleao1807@gmail.com>
2022-11-14 13:40:50 -0300

    feat: add middleware to validate if user has token

--- src/services/common/auth.ts
+++ src/services/common/auth.ts
@@ -1,10 +1,41 @@
-import { sign } from 'jsonwebtoken'
+/* eslint-disable @typescript-eslint/naming-convention */
+import { sign, verify } from 'jsonwebtoken'
 import { config } from '../../config'
 
 const createToken = ({ userId }: { userId: string }, options = { expiresIn: '1d' }) => {
   return sign({ user_id: userId }, config.AUTH_TOKEN, options)
 }
 
+const verifyJwt = (token: string) => {
+  try {
+    const payload = verify(token, config.AUTH_TOKEN)
+    return payload
+  } catch (e) {
+    return null
+  }
+}
+const parsePayload = (event) => {
+  const token = event.headers['x-api-key']
+  const user = verifyJwt(token)
+
+  return user
+}
+const authUser = (event) => {
+  const user = parsePayload(event)
+  return user
+}
+
+export const verifyAuth = async (req, res, next) => {
+  try {
+    const user = authUser(req)
+    if (typeof user === 'object' && user?.user_id) {
+      req.user = user.user_id
+    }
+    next()
+  } catch (error) {
+    return res.status(403).json({ message: 'Forbidden' })
+  }
+}
 const auth = {
   createToken
 }

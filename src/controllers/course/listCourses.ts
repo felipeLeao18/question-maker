82a95f5d5da00d0228d749d8b91c7aa74e874061
flp <flpleao1807@gmail.com>
2022-11-14 13:41:34 -0300

    feat: ensure user can list courses

---
+++ src/controllers/course/listCourses.ts
@@ -0,0 +1,45 @@
+
+import { Request, Response } from 'express'
+import { courseService } from '../../services/courseService'
+
+/**
+*  @swagger
+*  /courses:
+*  get:
+*    summary: list courses
+*    tags: [Course]
+*    consumes:
+*      - application/json
+*    parameters:
+*      - in: query
+*        name: filter
+*        description: or filter to contains on name or descriptiion
+*        schema:
+*          type: object
+*          required:
+*            - name
+*          properties:
+*            name:
+*              type: string
+*            description:
+*              type: string
+*    responses:
+*      422:
+*        description: 'Invalid schema'
+*      200:
+*        description: 'courses list'
+*        schema:
+*          $ref: '#/definitions/Course'
+*/
+
+export const listCourses = async (req: Request & { user: string }, res: Response) => {
+  const { filter, page, perPage } = req.query
+
+  const response = await courseService.list({
+    filter: filter?.toString() ?? undefined,
+    page: page ? parseInt(page.toString()) : undefined,
+    perPage: perPage ? parseInt(perPage.toString()) : undefined
+  },
+  req.user)
+  return res.status(200).json(response)
+}

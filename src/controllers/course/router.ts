38251463d1d9f377557ef2774b621b352e361739
flp <flpleao1807@gmail.com>
2022-11-15 18:15:16 -0300

    feat: add delete course route to course router

--- src/controllers/course/router.ts
+++ src/controllers/course/router.ts
@@ -3,10 +3,12 @@ import { Router } from 'express'
 import { verifyAuth } from '../../services/common/auth'
 import { createCourse } from './createCourse'
 import { listCourses } from './listCourses'
+import { removeCourse } from './removeCourse'
 
 const router = Router()
 
 router.post('/courses', verifyAuth, createCourse)
 router.get('/courses', verifyAuth, listCourses)
+router.delete('/courses', verifyAuth, removeCourse)
 
 export { router }

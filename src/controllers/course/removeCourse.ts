5073b7b67d5e546d333ece728f6cd339cb1b417b
flp <flpleao1807@gmail.com>
2022-11-15 18:15:27 -0300

    feat: add remove course controller

---
+++ src/controllers/course/removeCourse.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from 'express'
+import { courseService } from '../../services/courseService'
+
+/**
+*  @swagger
+*  /courses:
+*  delete:
+*    summary: Delete course
+*    tags: [Course]
+*    consumes:
+*      - application/json
+*    parameters:
+*      - in: body
+*        name: courseId
+*        description: course id to be removed
+*    responses:
+*      422:
+*        description: 'courseId not provided'
+*      401:
+*        description: 'Unauthorized'
+*      200:
+*        description: 'Course removed'
+*        schema:
+*          $ref: '#/definitions/Course'
+*/
+
+export const removeCourse = async (req: Request & { user: string }, res: Response) => {
+  const {courseId} = req.body
+
+  const response = await courseService.remove(courseId, req.user)
+  return res.status(200).json(response)
+}

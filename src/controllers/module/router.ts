eab4b13ffa9c0ce679dcde83b3f44e6811008b43
flp <flpleao1807@gmail.com>
2022-11-15 22:21:25 -0300

    feat: add get module router to modules router

--- src/controllers/module/router.ts
+++ src/controllers/module/router.ts
@@ -3,9 +3,11 @@
 import { Router } from 'express'
 import { verifyAuth } from '../../services/common/auth'
 import { createModule } from './createModule'
+import { getModule } from './getModule'
 
 const router = Router()
 
 router.post('/modules', verifyAuth, createModule)
+router.get('/modules/:moduleId', verifyAuth, getModule)
 
 export { router }

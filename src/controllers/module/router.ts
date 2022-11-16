d64ee07c514e8b82ce8eeea7c35525b0e81afe81
flp <flpleao1807@gmail.com>
2022-11-15 22:39:20 -0300

    feat: add remove module to modules router

--- src/controllers/module/router.ts
+++ src/controllers/module/router.ts
@@ -4,10 +4,12 @@ import { Router } from 'express'
 import { verifyAuth } from '../../services/common/auth'
 import { createModule } from './createModule'
 import { getModule } from './getModule'
+import { removeModule } from './removeModule'
 
 const router = Router()
 
 router.post('/modules', verifyAuth, createModule)
+router.delete('/modules', verifyAuth, removeModule)
 router.get('/modules/:moduleId', verifyAuth, getModule)
 
 export { router }

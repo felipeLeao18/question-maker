806972f34cbb69a55927ede5d1a9aa58f54b4a58
flp <flpleao1807@gmail.com>
2022-11-15 22:21:35 -0300

    feat: add get module controller

---
+++ src/controllers/module/getModule.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express'
+import { moduleService } from '../../services/moduleService'
+
+/**
+*  @swagger
+*  /modules/moduleId:
+*  get:
+*    summary: get module
+*    tags: [Module]
+*    consumes:
+*      - application/json
+*    parameters:
+*      - in: params
+*        name: moduleId
+*        description: module id to be found
+*    responses:
+*      401:
+*        description: 'Unauthorized'
+*      200:
+*        description: 'module found'
+*        schema:
+*          $ref: '#/definitions/Module'
+*/
+
+export const getModule = async (req: Request & { user: string }, res: Response) => {
+  const { moduleId } = req.params
+
+  const response = await moduleService.findById(moduleId ? moduleId.toString() : '', req.user)
+  return res.status(200).json(response)
+}

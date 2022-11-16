3e25c673cb5d47da41429bd68cd62b84f9a9679a
flp <flpleao1807@gmail.com>
2022-11-15 21:58:16 -0300

    feat: add module interface

---
+++ src/types/IModule.ts
@@ -0,0 +1,21 @@
+/**
+ * @swagger
+ * definitions:
+ *     Module:
+ *       properties:
+ *         name:
+ *           type: string
+ *         description:
+ *           type: string
+ *         course:
+ *           type: objectid
+ *       required:
+ *         - name
+ *         - course
+ *       type: object
+ */
+export interface IModule {
+  name: string
+  description: string
+  course: string
+}

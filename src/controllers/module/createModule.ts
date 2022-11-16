181666b17e9b5106fc4c6efafe14ad8074a21289
flp <flpleao1807@gmail.com>
2022-11-15 21:59:26 -0300

    feat: add create module controller

---
+++ src/controllers/module/createModule.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from 'express'
+import { moduleService } from '../../services/moduleService'
+
+/**
+*  @swagger
+*  /modules:
+*  post:
+*    summary: Create module
+*    tags: [Module]
+*    consumes:
+*      - application/json
+*    parameters:
+*      - in: body
+*        name: module
+*        description: Create module
+*        schema:
+*          type: object
+*          required:
+*            - name
+*          properties:
+*            name:
+*              type: string
+*            description:
+*              type: string
+*            course:
+*              type: string
+*    responses:
+*      422:
+*        description: 'Invalid schema'
+*      200:
+*        description: 'module created'
+*        schema:
+*          $ref: '#/definitions/Module'
+*/
+
+export const createModule = async (req: Request & { user: string }, res: Response) => {
+  const { name, description, courseId } = req.body
+
+  const response = await moduleService.create({ name, description }, courseId, req.user)
+  return res.status(200).json(response)
+}

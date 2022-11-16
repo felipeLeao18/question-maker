7e3021c4d44d99d814b91c589043cc50ee985a79
flp <flpleao1807@gmail.com>
2022-11-16 13:17:14 -0300

    feat: add db connection when starting server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 /* eslint-disable no-console */
 import { app } from '@app'
 import { config } from '@config'
+import { connect } from '@lib/db'
 
-app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
-})
+const initialize = async () => {
+  await connect()
+  app.listen(config.PORT, () => {
+    console.log(`Server running on port ${config.PORT}`)
+  })
+}
+
+initialize().catch(err => console.error(err))

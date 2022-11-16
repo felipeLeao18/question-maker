115f27187e77d7765263ca2c26ee1656f9227b6f
flp <flpleao1807@gmail.com>
2022-11-15 22:58:36 -0300

    feat: add list modules to modules router

--- src/controllers/module/router.ts
+++ src/controllers/module/router.ts
@@ -4,6 +4,7 @@ import { Router } from 'express'
 import { verifyAuth } from '../../services/common/auth'
 import { createModule } from './createModule'
 import { getModule } from './getModule'
+import { listModules } from './listModules'
 import { removeModule } from './removeModule'
 
 const router = Router()
@@ -11,5 +12,6 @@ const router = Router()
 router.post('/modules', verifyAuth, createModule)
 router.delete('/modules', verifyAuth, removeModule)
 router.get('/modules/:moduleId', verifyAuth, getModule)
+router.get('/modules', verifyAuth, listModules)
 
 export { router }

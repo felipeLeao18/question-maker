5307791da04fd8bcefc4c04eda37daf91c73cf82
flp <flpleao1807@gmail.com>
2022-11-16 13:17:26 -0300

    chore: add db url to config file

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,6 @@ dotenv.config()
 
 export const config = {
   PORT: process.env.PORT ?? 3000,
-  AUTH_TOKEN: process.env.AUTH_TOKEN ?? '5ce5ecba-608a-4561-932a-05e25b86f672'
+  AUTH_TOKEN: process.env.AUTH_TOKEN ?? '5ce5ecba-608a-4561-932a-05e25b86f672',
+  DATABASE_URL: process.env.DATABASE_URL ?? 'mongodb://localhost:27017/question-maker?readPreference=primary&appname=question-maker'
 }

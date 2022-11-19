288b286f9f110a419b21245f05494596c355b78e
flp <flpleao1807@gmail.com>
2022-11-19 13:37:09 -0300

    feat: add remove lesson controller

---
+++ src/controllers/lesson/removeLesson.ts
@@ -0,0 +1,33 @@
+
+import { Request, Response } from 'express'
+import { lessonService } from '@services/lessonService'
+
+/**
+*  @swagger
+*  /lessons:
+*  delete:
+*    summary: Delete lesson
+*    tags: [Lesson]
+*    consumes:
+*      - application/json
+*    parameters:
+*      - in: body
+*        name: lessonId
+*        description: lesson id to be removed
+*    responses:
+*      422:
+*        description: 'lessonId not provided'
+*      401:
+*        description: 'Unauthorized'
+*      200:
+*        description: 'lesson removed'
+*        schema:
+*          $ref: '#/definitions/Lesson'
+*/
+
+export const removeLesson = async (req: Request & { user: string }, res: Response) => {
+  const { lessonId } = req.body
+
+  const response = await lessonService.remove(lessonId, req.user)
+  return res.status(200).json(response)
+}

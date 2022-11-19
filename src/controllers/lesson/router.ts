212381bf1f8f3d4722072b4509326b862056a23b
flp <flpleao1807@gmail.com>
2022-11-19 13:17:36 -0300

    feat: add get lesson route to lesson router

--- src/controllers/lesson/router.ts
+++ src/controllers/lesson/router.ts
@@ -4,9 +4,11 @@ import { Router } from 'express'
 import { verifyAuth } from '@services/common/auth'
 import { createLesson } from './createLesson'
 import { listLessons } from './listLessons'
+import { getLesson } from './getLesson'
 const router = Router()
 
 router.post('/lessons', verifyAuth, createLesson)
 router.get('/lessons', verifyAuth, listLessons)
+router.get('/lessons/:lessonId', verifyAuth, getLesson)
 
 export { router }

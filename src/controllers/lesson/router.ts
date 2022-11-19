28e4508ae3453d445564f46c6637cce1b87f6775
flp <flpleao1807@gmail.com>
2022-11-19 12:08:43 -0300

    feat: add list lessons to lessons router

--- src/controllers/lesson/router.ts
+++ src/controllers/lesson/router.ts
@@ -3,8 +3,10 @@
 import { Router } from 'express'
 import { verifyAuth } from '@services/common/auth'
 import { createLesson } from './createLesson'
+import { listLessons } from './listLessons'
 const router = Router()
 
 router.post('/lessons', verifyAuth, createLesson)
+router.get('/lessons', verifyAuth, listLessons)
 
 export { router }

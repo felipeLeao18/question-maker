86802a3f25ddf2d7fe59c7d9802338ed2f32d061
flp <flpleao1807@gmail.com>
2022-11-19 13:17:47 -0300

    feat: add get lesson controller

---
+++ src/controllers/lesson/getLesson.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express'
+import { lessonService } from '@services/lessonService'
+
+/**
+*  @swagger
+*  /lessons/lessonId:
+*  get:
+*    summary: get lesson
+*    tags: [Lesson]
+*    consumes:
+*      - application/json
+*    parameters:
+*      - in: params
+*        name: lessonId
+*        description: lesson id to be found
+*    responses:
+*      401:
+*        description: 'Unauthorized'
+*      200:
+*        description: 'lesson found'
+*        schema:
+*          $ref: '#/definitions/Lesson'
+*/
+
+export const getLesson = async (req: Request & { user: string }, res: Response) => {
+  const { lessonId } = req.params
+
+  const response = await lessonService.findById(lessonId ? lessonId.toString() : '', req.user)
+  return res.status(200).json(response)
+}

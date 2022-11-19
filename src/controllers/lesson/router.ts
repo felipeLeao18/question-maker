9dcd4d7a987208c3515e409ff3b515516cde786e
flp <flpleao1807@gmail.com>
2022-11-19 13:36:53 -0300

    feat: add delete lesson route to lessons router

--- src/controllers/lesson/router.ts
+++ src/controllers/lesson/router.ts
@@ -5,10 +5,12 @@ import { verifyAuth } from '@services/common/auth'
 import { createLesson } from './createLesson'
 import { listLessons } from './listLessons'
 import { getLesson } from './getLesson'
+import { removeLesson } from './removeLesson'
 const router = Router()
 
 router.post('/lessons', verifyAuth, createLesson)
 router.get('/lessons', verifyAuth, listLessons)
 router.get('/lessons/:lessonId', verifyAuth, getLesson)
+router.delete('/lessons', verifyAuth, removeLesson)
 
 export { router }
